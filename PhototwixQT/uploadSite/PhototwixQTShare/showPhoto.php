18929dd8067e0a9effea6213eda928da869ca132
Rathouis Charles <charles.rathouis@gmail.com>
2015-10-29 18:52:08 +0100

    Update website

--- PhototwixQT/uploadSite/PhototwixQTShare/showPhoto.php
+++ PhototwixQT/uploadSite/PhototwixQTShare/showPhoto.php
@@ -1,11 +1,15 @@
-
-
 <html>
 	<head>
 		<meta charset="utf-8" />
         <title>Phototwix</title>
-		<script src="./lib/oauth.min.js"></script>
+		
 		<link rel="stylesheet" type="text/css" href="css/mainstyle.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css" />
+		<link rel="stylesheet" type="text/css" href="css/flavr.css" />
+		
+		<script type="text/javascript" src="lib/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="lib/oauth.min.js"></script>
+		<script type="text/javascript" src="lib/flavr.js"></script>
 		
 		<script>
 			
@@ -15,6 +19,7 @@
 <?php
 			$imagePath = "";
 			$event_code = "";
+			
 
 			function cut_string_using_last($character, $string, $side, $keep_character=true) { 
 				$offset = ($keep_character ? 1 : 0); 
@@ -49,17 +54,34 @@
 				echo "No event";
 			}
 
-			$actual_link_base =  "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-
+			$actual_link_base = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+			if (strpos($actual_link_base, "&callback")) {
+				$actual_link_base = strstr($actual_link_base, "&callback", true);
+			}
+			
 			$actual_link = cut_string_using_last('/', $actual_link_base, 'left', true);
 			$actual_link = $actual_link."photos/".$imagePath;
 			$actual_link_encode = urlencode($actual_link);
+			
+			//Encode to base64
+			
+			$path = "photos/".$imagePath;
+			$type = pathinfo($path, PATHINFO_EXTENSION);
+			$data = file_get_contents($path);
+			$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
 ?>			
+			var event_code = <?php echo "'$event_code'"; ?>
 			
-			
-			var picUrl = encodeURIComponent("<?php echo $actual_link;?>")
+			var picUrl = "<?php echo $actual_link;?>"
+
 			var redirectUrl = encodeURIComponent("<?php echo $actual_link_base; ?>")
+			
+			var facebook_message = "Test facebook"
+			var twitter_message = "Test twitter"
+			var google_message = "Test google"
+			
+			
 			
 			function postFacebook() {
 				OAuth.redirect('facebook', {cache: true}, "<?php echo $actual_link_base."&callback=facebook"; ?>");	
@@ -79,10 +101,83 @@
 				OAuth.redirect('google_plus', {cache: false}, "<?php echo $actual_link_base."&callback=google_plus"; ?>");
 			}
 			
-			function postEmail() {
-				
+			function enterEmail() {
+				new $.flavr({
+					content     : 'Votre email ?',
+					dialog      : 'prompt',
+					prompt      : { placeholder: 'email' },
+					onConfirm   : function( $container, $prompt ){
+						saveMail($prompt.val());
+						sendMail($prompt.val());
+					},
+					onCancel    : function(){
+						
+					}
+				});
 			}
 			
+			function sendMail(mail) {
+				console.log("Envoie d'un email" + mail);
+				//TODO
+				confirmSend();
+			}
+			
+			function confirmSend() {
+				new $.flavr({
+					content     : 'La photo est envoyée !',
+					autoclose   : true,
+					timeout     : 2000  /* Default timeout is 3 seconds */
+				});
+			}
+			
+			function errorSend() {
+				new $.flavr({
+					content     : 'Une erreur est survenue !',
+					autoclose   : true,
+					timeout     : 2000  /* Default timeout is 3 seconds */
+				});
+			}
+			
+			function saveFacebook(res) {
+				post_data = {
+					'connection_type' 		: 'facebook',
+					'connection_name'		: res.name,
+					'connection_mail'		: res.email,
+					'connection_gender'		: res.gender,
+					'connection.birthday'	: res.birthday
+				};
+				console.log('Post Facebook: ' + JSON.stringify(post_data));
+			}
+			
+			function saveTwitter(res) {
+				post_data = {
+					'connection_type' 		: 'twitter',
+					'connection_name'		: res.name,
+					'connection_mail'		: res.email,
+					'connection_gender'		: res.gender,
+					'connection.birthday'	: res.birthday
+				};
+				console.log('Post Twitter: ' + JSON.stringify(post_data));
+			}			
+			
+			function saveGoogle(res) {
+				post_data = {
+					'connection_type' 		: 'twitter',
+					'connection_name'		: res.name,
+					'connection_mail'		: res.email,
+					'connection_gender'		: res.gender,
+					'connection.birthday'	: res.birthday
+				};
+				console.log('Post Google: ' + JSON.stringify(post_data));
+			}
+			
+			function saveMail(res) {
+				post_data = {
+					'connection_type' 		: 'mail',
+					'connection_mail'		: res
+				};
+				console.log('Post Mail: ' + JSON.stringify(post_data));
+			}
 			
 			OAuth.initialize('3P-A917vxAg-qsXtHy4MtGjiH2Q');
 
@@ -93,10 +188,10 @@
 			if ("google_plus" == callback_res) {
 				OAuth.callback('google_plus', {cache: false})
 					.done(function(result) {
-					  console.log("Tachatte");
 					  result.me()
 						.done(function (response) {
 							console.log('/me response: ' + JSON.stringify(response));
+							saveGoogle(response);
 						})
 						.fail(function (err) {
 							console.log('/me error: ' + JSON.stringify(err));
@@ -129,11 +224,41 @@
 					  result.get('/1.1/account/verify_credentials.json')
 						.done(function (response) {
 							console.log('/me response: ' + JSON.stringify(response));
+							saveTwitter(response);
 						})
 						.fail(function (err) {
 							console.log('/me error: ' + JSON.stringify(err));
 						})
-					})
+					  
+					  /*
+					  result.post('/1.1/statuses/update_with_media.json', {
+						  'status' : twitter_message,
+						  'media[]' : ['<?php echo $base64; ?>']
+					  }).done(function (response) {
+								confirmSend();
+							})
+							.fail(function (error) {
+								console.log(JSON.stringify(error));
+								errorSend() 
+							});
+						})
+						.fail(function (error) {
+							console.log(JSON.stringify(error));
+							errorSend() 
+						})
+					  */
+
+					  var url = 'http://media.giphy.com/media/Qw4X3FHCPlgDCDnMebe/giphy.gif'
+					  var message = encodeURIComponent(twitter_message + ' ' + url);
+					  result.post('1.1/statuses/update.json?status=' + message)
+						.done(function (response) {
+							confirmSend();
+						})
+						.fail(function (error) {
+							console.log(JSON.stringify(err));
+							errorSend() 
+						});
+
 					.fail(function (error) {
 							console.log('Error connect twitter	' + JSON.stringify(error));
 					});
@@ -145,32 +270,33 @@
 					  result.get('/me?fields=name,email,gender,birthday,hometown,interested_in,languages,last_name,cover')
 						.done(function (response) {
 							console.log('/me response: ' + JSON.stringify(response));
+							saveFacebook(response);
+							confirmSend();
 						})
-						.fail(function (err) {
-							console.log('/me error: ' + JSON.stringify(err));
-						});
+						.fail(function (error) {
+							console.log('/me error: ' + JSON.stringify(error));
+							errorSend() 
+						});	
 						
-					  result.get('/me/permissions')
-					  .done(function (response) {
-							console.log('/me/permissions: ' + JSON.stringify(response));
-						})
-					/*	
-					  result.post('/me/feed', {
+						result.post('/me/feed', {
 							data: {
-								message: 'Phototwix test!',
-								link: "http://www.vargajeanclaude.be/medias/images/iamnotanartist-gifparanoia-08.gif"
+								message: facebook_message,
+								link: 'http://media.giphy.com/media/Qw4X3FHCPlgDCDnMebe/giphy.gif'//,
+								//link: picUrl
 							}
 						})
 						.done(function (response) {
-							console.log('/me/feed response: ' + JSON.stringify(response));
+							confirmSend();
 						})
 						.fail(function (error) {
-							console.log('/me/feed error: ' + JSON.stringify(err));
+							console.log('/me/feed error: ' + JSON.stringify(error));
+							errorSend() 
 						});
-					*/
+						
 					})
-					.fail(function (err) {
-					  console.log('Failed to connect to Facebook')
+					.fail(function (error) {
+					  console.log('Failed to connect to Facebook');
+					  errorSend();
 					});			
 			}
 			
@@ -183,10 +309,10 @@
 
 			<div id="socialButtonGroup">
 				<img class="socialButton" src="img/facebook.png" onclick="postFacebook()"></img>
-				<img class="socialButton" src="img/google-logo.png" onclick="postGooglePlus()"></img>
-				<img class="socialButton" src="img/instagram.png" onclick="postInstagram()"></img>
+				<!--img class="socialButton" src="img/google-logo.png" onclick="postGooglePlus()"></img-->
+				<!--img class="socialButton" src="img/instagram.png" onclick="postInstagram()"></img-->
 				<img class="socialButton" src="img/twitter.png" onclick="postTwitter()"></img>
-				<img class="socialButton" src="img/emailbutton.jpg" onclick="postEmail()"></img>
+				<img class="socialButton" src="img/emailbutton.jpg" onclick="enterEmail()"></img>
 			</div>
 		</div>
 		
